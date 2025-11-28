1de9476449c77ffecdf64ecbf03b3def0d1acc1f
Fabio Meireles <fbomrl14@gmail.com>
2025-11-28 12:10:38 -0300

    fix: include two new functions in creatureMoveRepository

--- internal/repository/creatureMoveRepository.go
+++ internal/repository/creatureMoveRepository.go
@@ -17,3 +17,35 @@ func (repo *CreatureMoveRepository) AddMoveToCreature(creatureId int, moveId int
 
 	return err
 }
+
+func (repo *CreatureMoveRepository) CreatureExists(id int) (bool, error) {
+	var creatureExists
+	err := r.DB.QueryRow(
+		"SELECT 1 FROM CREATURE WHERE id = ? LIMIT 1",
+		id,
+	).Scan(&creatureExists)
+	
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return, err
+		}
+	}	
+
+	return err, nil
+}
+
+func (repo *CreatureMoveRepository) MoveExists(id int) (bool, err) {
+	var moveExists
+	err := r.DB.QueryRow(
+		"SELECT 1 FROM MOVES WHERE id = ? LIMIT 1",
+		id,
+	)Scan(&moveExists)
+
+	if err != nil {
+		if err ==  sql.ErrNoRows {
+			return, err
+		}
+	}
+
+	return err, nil
+}
