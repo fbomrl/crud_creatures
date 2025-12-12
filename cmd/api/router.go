ece163d781caa1c240059c0fe3f6a6727c07d157
Fabio Meireles <fbomrl14@gmail.com>
2025-12-12 10:19:09 -0300

    fix: create routers in router.go

--- cmd/api/router.go
+++ cmd/api/router.go
@@ -1,1 +1,19 @@
 package api
+
+import (
+	"crud_creatures/internal/services"
+	"net/http"
+)
+
+func Routers(moveService *services.MoveService, creatureService *services.CreatureService) http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/creatures", http.StatusFound)
+	})
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	return mux
+}
